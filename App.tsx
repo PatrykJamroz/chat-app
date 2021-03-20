5d5e83b2c0093dc7ef49a7130ea9e67bf06e4153
Patryk J <jamroz.patryk@gmail.com>
2021-03-20 10:17:44 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Chat from "./components/Chat";
+import { ApolloProvider, useQuery, gql } from "@apollo/client";
+import { useState } from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Room list:</Text>
+      <Chat />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  roomPic: {
+    width: 50,
+    height: 50,
   },
 });
