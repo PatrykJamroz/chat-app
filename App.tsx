7be9e5fe0d8fa75ce5dfa0d345ee925a3b0642a6
Patryk J <jamroz.patryk@gmail.com>
2021-03-20 20:47:15 +0100

    displays messages of one room

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
 // import Chat from "./components/Chat";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
@@ -63,8 +63,10 @@ const GET_ROOMS = gql`
   }
 `;
 
+// room(id: "33290044-5232-46be-9302-210f5291905b")
+
 const GET_MESSAGES = gql`
-  query Room($roomID: String!) {
+  query GetMessages($roomID: String!) {
     room(id: $roomID) {
       id
       messages {
@@ -95,48 +97,100 @@ const GET_MESSAGES = gql`
 `;
 
 ///
+let roomIDS = [];
 
 function HomeScreen({ navigation }) {
   const { data, loading, error } = useQuery(GET_ROOMS);
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error :(</Text>;
-  return data.usersRooms.rooms.map((room: Room) => (
+  roomIDS = data.usersRooms.rooms.map((room: Room) => {
+    return room.id;
+  });
+  console.log(roomIDS);
+  return data.usersRooms.rooms.map((room: Room, index) => (
     <View key={room.id}>
-      <Image style={styles.roomPic} source={{ uri: room.roomPic }} />
+      <Image
+        style={styles.roomPic}
+        source={{
+          uri:
+            room.roomPic !== ""
+              ? room.roomPic
+              : "https://lh3.googleusercontent.com/proxy/eLBl-oqqyz7uPHWUOGLBY_ZxXCcOjru-ggO5QfNq3DZeIOUXYYwuj0YZoMhStr-gft55iMF6kfDmfG6l7fXzyxdBl6QzFoN5Wgd0RdU",
+        }}
+      />
       <Text>{room.name}</Text>
       <Button
         title="Go to room"
-        onPress={() =>
-          navigation.navigate("Room", {
-            roomID: "33290044-5232-46be-9302-210f5291905b",
-          })
-        }
+        onPress={() => {
+          navigation.navigate("Room", { roomID: room.id });
+        }}
       />
     </View>
   ));
 }
 
 function RoomScreen(route) {
-  const roomID = route.params;
+  // const { roomID } = route.params;
+  const roomID = "33290044-5232-46be-9302-210f5291905b";
   const { data, loading, error } = useQuery(GET_MESSAGES, {
-    variables: roomID,
+    variables: { roomID: roomID },
   });
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error :(</Text>;
 
-  return data.room.messages.map((message) => (
-    <View key={message.id}>
-      <Image style={styles.roomPic} source={{ uri: message.user.profilePic }} />
-      <Text>
-        {message.user.firstName}: {message.body}
-      </Text>
+  return (
+    <View>
+      {data.room.messages.map((message) => (
+        <View key={message.id}>
+          <Image
+            style={styles.roomPic}
+            source={{
+              uri:
+                message.user.profilePic !== ""
+                  ? message.user.profilePic
+                  : "https://www.uclg-planning.org/sites/default/files/styles/featured_home_left/public/no-user-image-square.jpg?itok=PANMBJF-",
+            }}
+          />
+          <Text>
+            {message.user.firstName}: {message.body}
+          </Text>
+        </View>
+      ))}
+      <TextInput style={styles.input} />
     </View>
-  ));
+  );
 }
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  // let roomIDs = [];
+
+  // function getRoomsID() {
+  //   const data = client.readFragment({
+  //     rooms
+  //     fragment: gql`
+  //       {
+  //         usersRooms {
+  //           rooms {
+  //             id
+  //             name
+  //             roomPic
+  //           }
+  //         }
+  //       }
+  //     `,
+  //   });
+  //   roomIDs = data.usersRooms.rooms.map((room: Room) => {
+  //     return room.id;
+  //   });
+  // }
+
+  // useEffect(() => {
+  //   getRoomsID();
+  //   console.log(roomIDs);
+  // }, []);
+
   return (
     <ApolloProvider client={client}>
       <NavigationContainer>
@@ -163,5 +217,11 @@ const styles = StyleSheet.create({
   roomPic: {
     width: 50,
     height: 50,
+    borderRadius: "50%",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
