14fb4003669d139e6f291ba5caca81577a8ed73e
Patryk J <jamroz.patryk@gmail.com>
2021-04-17 22:41:17 +0200

    subscription does not work, WS conn failed

--- components/RoomScreen.tsx
+++ components/RoomScreen.tsx
@@ -4,6 +4,7 @@ import "react-native-gesture-handler";
 import {
   /*GET_MESSAGES,*/ MESSAGE_SUBSCRIPTION,
   POST_MESSAGE,
+  GET_ROOMS,
 } from "./Querries";
 import { useQuery, useMutation } from "@apollo/client";
 // import PostMessage from "./PostMessage";
@@ -14,22 +15,21 @@ import { login } from "../misc/crede";
 export default function RoomScreen(props) {
   const roomID = props.route.params.roomID;
   const [postMessage] = useMutation(POST_MESSAGE);
-  const { data, loading, error, subscribeToMore } = useQuery(GET_MESSAGES, {
-    variables: { roomID: roomID },
-  });
+  const { data, loading, error, subscribeToMore } = useQuery(GET_ROOMS);
 
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error :(</Text>;
 
   subscribeToMore({
     document: MESSAGE_SUBSCRIPTION,
-    variables: { roomID: roomID },
+    // variables: { roomID: roomID },
     updateQuery: (prev, { subscriptionData }) => {
       if (!subscriptionData.data) return prev;
-      const newFeedItem = subscriptionData.data.messageAdded;
+      const newFeedItem = subscriptionData.data.rooms;
       return Object.assign({}, prev, {
-        room: {
-          messages: [...prev.room.messages, newFeedItem],
+        rooms: {
+          // messages: [...prev.room.messages, newFeedItem],
+          newFeedItem,
         },
       });
     },
@@ -38,26 +38,26 @@ export default function RoomScreen(props) {
   return (
     <View style={styles.container}>
       <GiftedChat
-        messages={data.room.messages.map((msg) => ({
-          _id: msg.id,
+        messages={data.rooms[roomID].messages.map((msg) => ({
+          // _id: msg.id,
           text: msg.body,
           user: {
-            _id: msg.user.email,
-            name: msg.user.firstName,
+            _id: msg.user.name,
+            name: msg.user.name,
             avatar: msg.user.profilePic,
           },
         }))}
         onSend={(e) => {
           postMessage({
             variables: {
-              messageBody: e[0].text,
+              body: e[0].text,
               roomID: roomID,
-              email: login.email,
-              password: login.password,
+              user: "User",
+              // password: login.password,
             },
           });
         }}
-        user={{ _id: login.email }}
+        user={{ _id: "User" }}
         inverted={false}
         showUserAvatar={false}
         renderUsernameOnMessage={true}
