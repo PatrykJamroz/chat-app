7de825576bf76ad8a08ccdd54534b9b4f2cdb6a0
Patryk J <jamroz.patryk@gmail.com>
2021-04-18 11:35:13 +0200

    changed apollo setup

--- components/ApolloSetup.ts
+++ components/ApolloSetup.ts
@@ -5,6 +5,7 @@ import { createAbsintheSocketLink } from "@absinthe/socket-apollo-link";
 import { Socket as PhoenixSocket } from "phoenix";
 import { hasSubscription } from "@jumpn/utils-graphql";
 import { split } from "apollo-link";
+import { WebSocketLink } from "@apollo/client/link/ws";
 
 import { token } from "../misc/crede";
 
@@ -23,28 +24,36 @@ const httpLink = createHttpLink({
 
 // const authedHttpLink = authLink.concat(httpLink);
 
-const phoenixSocket = new PhoenixSocket("ws://localhost:4000", {
-  params: {} /*() => {
-    return {
-      // token: token,
-    };
-  },*/,
+// const phoenixSocket = new PhoenixSocket("ws://localhost:4000", {
+//   params: {} /*() => {
+//     return {
+//       // token: token,
+//     };
+//   },*/,
+// });
+
+// const absintheSocket = AbsintheSocket.create(phoenixSocket);
+
+// const websocketLink = createAbsintheSocketLink(absintheSocket);
+
+const websocketLink = new WebSocketLink({
+  uri: "ws://localhost:4000/",
+  options: {
+    reconnect: true,
+  },
 });
 
-const absintheSocket = AbsintheSocket.create(phoenixSocket);
-
-const websocketLink = createAbsintheSocketLink(absintheSocket);
-
-const link = split(
-  (operation) => hasSubscription(operation.query),
-  websocketLink,
-  // authedHttpLink
-  httpLink
-);
+// const link = split(
+//   (operation) => hasSubscription(operation.query),
+//   websocketLink,
+//   // authedHttpLink
+//   httpLink
+// );
 
 const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-  link,
+  websocketLink,
+  uri: "http://localhost:4000/",
   cache,
 });
