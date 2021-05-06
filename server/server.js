72021994778d8de48dc24b2961d2c00a9f268463
Patryk Jamróz <jamroz.patryk@gmail.com>
2021-05-06 18:20:23 +0200

    changed server.ts into js due to nodemon installation. faster to run w/o ts

--- server/server.js
+++ server/server.js
@@ -91,17 +91,21 @@ const resolvers = {
   },
   Subscription: {
     messageAdded: {
-      subscribe: (parent, args, { pubsub, name, body }) => {
+      subscribe: (parent, args, { pubsub }) => {
         const channel = Math.random().toString(36).slice(2, 15);
         onRoomsUpdates(() =>
           pubsub.publish(channel, {
-            messages: [{ user: { name: name, profilePic: "" }, body: body }],
+            messageAdded: [
+              { user: { name: "name", profilePic: "" }, body: "body" },
+            ],
           })
         );
         setTimeout(
           () =>
             pubsub.publish(channel, {
-              messages: [{ user: { name: name, profilePic: "" }, body: body }],
+              messageAdded: [
+                { user: { name: "name", profilePic: "" }, body: "body" },
+              ],
             }),
           0
         );
